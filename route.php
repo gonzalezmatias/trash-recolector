6bcf4696a4ef077ff929734e7e0d998d9706cd7c
Belen Enemark <belenenemark@gmail.com>
2019-04-30 17:42:54 -0300

    Archivos htaccess y route

---
+++ route.php
@@ -0,0 +1,41 @@
+<?php
+
+//esto ya esta agregado todo
+require_once "config/ConfigApp.php";
+require_once "controller\HomeController.php";
+require_once "controller\FormController.php";
+
+
+
+
+function parseURL($url)
+{
+  $urlExploded = explode('/', $url);
+  $arrayReturn[ConfigApp::$ACTION] = $urlExploded[0];
+
+  #borrar/1/2/3/4
+  $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[1]) ? array_slice($urlExploded,1) : null;
+  return $arrayReturn;
+}
+
+if(isset($_GET['action'])){
+
+    $urlData = parseURL($_GET['action']);
+    $action = $urlData[ConfigApp::$ACTION]; //home
+    if(array_key_exists($action,ConfigApp::$ACTIONS)){
+        $params = $urlData[ConfigApp::$PARAMS];
+        $action = explode('#',ConfigApp::$ACTIONS[$action]);
+        $controller =  new $action[0]();
+        $metodo = $action[1];
+        if(isset($params) &&  $params != null){
+            echo $controller->$metodo($params);
+        }
+        else{
+            echo $controller->$metodo();
+        }
+    }else{
+      $controller =  new HomeController();
+      echo $controller->Home();
+    }
+}
+ ?>
