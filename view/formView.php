bc994e06dae46f0404ae1c2506d2492ebbe0d19a
Facundo Genaro <facundogenaro@hotmail.com>
2019-05-02 00:52:01 -0300

    Bug fix botón coordenadas

--- view/formView.php
+++ view/formView.php
@@ -2,7 +2,7 @@
 <?php
 require('libs/Smarty.class.php');
 
-// Cuando el controller lo solicita, muestra el template requerido. 
+// Cuando el controller lo solicita, muestra el template requerido.
 class FormView
 {
 
@@ -16,7 +16,7 @@ class FormView
 
   }
 
-  function mostrar($titulo){
+  function mostrar($titulo){ //La activación de la función pone en pantalla el template solicitado
     $this->Smarty->assign('titulo',$titulo);
     $this->Smarty->display('templates/form.tpl');
   }
